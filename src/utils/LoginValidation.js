84e44b1ff73d0331fd3263c78a4c34e80e8b47f5
scherpablo <scherpablo@gmail.com>
2023-11-19 12:36:59 -0300

    update create form z-index

--- src/utils/LoginValidation.js
+++ src/utils/LoginValidation.js
@@ -4,9 +4,6 @@ const validateEmail = (email) => {
 };
 
 const validatePassword = (password) => {
-  // const passwordRegex =
-  //   /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,16}$/;
-  // Esta es la expresión regular modificada para incluir la validación de un símbolo
   const passwordRegex =
     /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,16}$/;
   return passwordRegex.test(password);
